e68ff4360106475a7f0cfeeb8aeaf57da4ef07c7
Rex McKinnon <rex@mckinnon.ninja>
2024-01-25 18:11:23 -0800

    Manually inlined some functions to save flash.

--- src/main.c
+++ src/main.c
@@ -9,9 +9,13 @@
 #include "boot_config.h"
 #include "bootloader.h"
 
-// Util functions for setting/unsetting state.
-void clock_set (void);
-void clock_reset (void);
+#include "nvm.h"
+
+// Configuration words for software control of watchdog timer.
+#pragma config WDTCCS   = SC
+#pragma config WDTCWS   = WDTCWS_7
+#pragma config WDTE     = SWDTEN
+#pragma config WDTCPS   = WDTCPS_31
 
 // These variables hold the POR values of registers we use.
 static volatile unsigned char por_osc = 0;
@@ -20,57 +24,58 @@ static volatile unsigned char por_hfosc = 0;
 void
 main (void)
 {
-    // We only start boot loader if RX is high.
-    if (BOOTLOADER_PIN)
+    // Watchdog was not cause of reset, meaning we start bootloader
+    if (1 == PCON0bits.nRWDT)
     {
-        clock_set();
+
+        // Set watchdog prescaler to ~16s
+        WDTCON0bits.WDTPS = 0b01110;
+
+        // Enable WDT
+        WDTCON0bits.SWDTEN = 1;
+
+        // unsigned char new_osc = 0;
+
+        // Save value of clock:divider at POR
+        por_osc = OSCCON2;
+
+        // Save value of HFINTOSC
+        por_hfosc = OSCFRQ;
+
+        // if (OSCCON2bits.COSC != 0b110)
+        // {
+        //     // Set clock source to HFINTOSC if not already.
+        //     new_osc |= (0b110 << 4);
+        // }
+
+        // if (OSCCON2bits.CDIV != 0)
+        // {
+            // Set clock divider to 1:1 if not
+            // (Default value of new_osc)
+            // new_osc |= 0;
+        // }
+
+        // Set NOSC register to HFINTOSC with 1:1 divider.
+        OSCCON1 = 0b1100000;
+
+        // Set HFINTOSC to our needed speed (20 mhz?)
+        OSCFRQ = 0b110;
+
         bootloader_start();
-        clock_reset();
+
+        //  Reset used registers back to POR value
+        OSCCON1 = por_osc;
+        OSCFRQ = por_hfosc;
+
+        // Disable watchdog
+        WDTCON0bits.SWDTEN = 0;
+
+        // Reset prescaler
+        WDTCON0bits.WDTPS = 0b01011;
     }
 
     // Jump to application reset vector
     asm("ljmp " ___mkstr(PROG_RST_VECT));
-
-    while (1);
-}
-
-void
-clock_set (void)
-{
-    unsigned char new_osc = 0;
-
-    // Save value of clock:divider at POR
-    por_osc = OSCCON2;
-
-    // Save value of HFINTOSC
-    por_hfosc = OSCFRQ;
-
-    if (OSCCON2bits.COSC != 0b110)
-    {
-        // Set clock source to HFINTOSC if not already.
-        new_osc |= (0b110 << 4);
-    }
-
-    if (OSCCON2bits.CDIV != 0)
-    {
-        // Set clock divider to 1:1 if not
-        // (Default value of new_osc)
-        // new_osc |= 0;
-    }
-
-    // Set NOSC register with our new clock:divider value.
-    OSCCON1 = new_osc;
-
-    // Set HFINTOSC to our needed speed
-    OSCFRQ = 0b110;
-}
-
-void
-clock_reset (void)
-{
-    //  Reset used registers back to POR value
-    OSCCON1 = por_osc;
-    OSCFRQ = por_hfosc;
 }
 
 // EOF //
